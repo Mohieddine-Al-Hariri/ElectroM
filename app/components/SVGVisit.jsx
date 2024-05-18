9bb1fdc9dc514afc47c6566459f39e635465eb8c
Mohieddine-Al-Hariri <114818981+Mohieddine-Al-Hariri@users.noreply.github.com>
2024-05-18 11:33:39 +0300

    Cart Item
    Better Responsivity for mobile
    Can now visit the product when the title is pressed.

---
+++ app/components/SVGVisit.jsx
@@ -0,0 +1,14 @@
+const SVGVisit = (props) => (
+  <svg
+    fill="currentColor"
+    width="15px"
+    height="15px"
+    viewBox="0 0 6.4 6.4"
+    id="Flat"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path d="m3.1 3.6 0 1.5a0.3 0.3 0 0 1 -0.6 0l0 -0.776 -1.288 1.288a0.3 0.3 0 1 1 -0.424 -0.424l1.288 -1.288L1.3 3.9A0.3 0.3 0 0 1 1.3 3.3h0l1.5 0a0.3 0.3 0 0 1 0.3 0.3M5.2 0.7H1.8a0.501 0.501 0 0 0 -0.5 0.5v1.2a0.3 0.3 0 0 0 0.6 0V1.3h3.2v3.2h-1.1a0.3 0.3 0 0 0 0 0.6h1.2a0.501 0.501 0 0 0 0.5 -0.5V1.2a0.501 0.501 0 0 0 -0.5 -0.5" />
+  </svg>
+);
+export default SVGVisit;
